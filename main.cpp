0a0df90a4ef8d1e6c6b5055a8bc65b0033ec4f01
LevTrachuk <155105125+LevTrachuk@users.noreply.github.com>
2024-05-07 14:39:16 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "BMP.h"
+
+int main() {
+
+    BMP bmp1("im.bmp");
+    //Поворот на 90 градусов по часовой стрелке
+    bmp1.rotate_clockwise_90();
+    bmp1.write("output_clockwise.bmp");
+
+    BMP bmp2("im.bmp");
+    //Против часовой
+    bmp2.rotate_counterclockwise_90();
+    bmp2.write("output_counterclockwise.bmp");
+
+    return 0;
+}
