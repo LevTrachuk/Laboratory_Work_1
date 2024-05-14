fb5bde9753561d2a0229c3ec12ac0d7fe06ea57c
LevTrachuk <155105125+LevTrachuk@users.noreply.github.com>
2024-05-14 09:22:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,15 @@
 
 int main() {
 
-    BMP bmp1("image_.bmp");
+    BMP img1("image_.bmp");
     //Процесс поворота на 90 градусов по часовой стрелке
-    bmp1.rotate_clockwise_90();
-    bmp1.write("output_clockwise.bmp");
+    img1.rotate_clockwise_90();
+    img1.write("output_clockwise.bmp");
 
-    BMP bmp2("image_.bmp");
+    BMP img2("image_.bmp");
     //Процесс поворота на 90 градусов против часовой стрелки
-    bmp2.rotate_counterclockwise_90();
-    bmp2.write("output_counterclockwise.bmp");
+    img2.rotate_counterclockwise_90();
+    img2.write("output_counterclockwise.bmp");
 
     return 0;
 }
